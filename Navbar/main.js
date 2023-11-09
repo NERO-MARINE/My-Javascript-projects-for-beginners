e0520be7b43608de53c5a69a7780ef8e834ba518
Oghenero Okerri <oghenerokerri@gmail.com>
2023-11-09 15:15:06 +0100

    Added new projects

--- Navbar/main.js
+++ Navbar/main.js
@@ -1,9 +1,6 @@
-const toggler = document.querySelector('.toggler');
-const navbarlinks = document.querySelector('.nav-links');
-const navbarSearch = document.querySelector('input[type="search"]');
+const hambuger = document.querySelector('.hambuger');
+const navLinks = document.querySelector('.navbar_links');
 
-toggler.addEventListener('click', ()=> {
-    navbarlinks.classList.toggle('active');
-    toggler.classList.toggle('toggler_small_screen');
-    navbarSearch.classList.toggle('active');
-});
+hambuger.addEventListener('click', ()=>{
+    navLinks.classList.toggle('active')
+})
---
+++ curtain_nav/main.js
@@ -0,0 +1,8 @@
+const hambuger = document.querySelector('.hambuger');
+const navbar = document.querySelector('.Navbar');
+
+hambuger.addEventListener('click',()=>{
+    navbar.classList.toggle('active')
+    hambuger.classList.toggle('active')
+    
+})
